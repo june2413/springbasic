b245dc10ab11e592c7ba8f20dd01501505f0bb94
june2413 <80863126+june2413@users.noreply.github.com>
2021-05-28 17:55:23 +0900

    ioc, di 기본

---
+++ june/spring/beans/HelloSpring5Bean03Eng.java
@@ -0,0 +1,10 @@
+package june.spring.beans;
+
+public class HelloSpring5Bean03Eng implements HelloSpring5Bean03 {
+
+	@Override
+	public void sayHello(String msg) {
+		System.out.println("Hello, " + msg);	
+	}
+
+}
